cdc58204a9cd306c7f895160ff4703bba4d868c8
AsilenceBTF <951208871@qq.com>
2021-12-14 16:42:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # klib
+
+实现AbstractMachine项目中一个基础运行库klib
+其中包含一系列基础库函数
---
+++ build/x86_64-qemu/src/stdio.d
@@ -0,0 +1,7 @@
+/home/asilencebtf/Public/os-workbench/abstract-machine/klib/build/x86_64-qemu/src/stdio.o: \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/klib/src/stdio.c \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/am/include/am.h \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/am/include/amdev.h \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/klib/include/klib.h \
+ /home/asilencebtf/Public/os-workbench/abstract-machine/klib/include/klib-macros.h
